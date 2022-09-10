e256065ec67e3162d90dff5aacd1da0df36d6ae8
romance <66000596+romance999@users.noreply.github.com>
2022-09-10 15:00:06 -0400

    get and print cpu vendor string

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,10 +1,17 @@
 #include "print.h"
+#include "types.h"
 
 void kernel_main() 
 {
+    // get cpu verdor
+    char vendor_str[13];
+    CPUID_vendor_string(vendor_str);
+
     print_clear();
     print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     printf("                ************DeathOS 64 bit kernel************");
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
     printf("\n KERNEL V0.01");
+    printf("\n CPU ");
+    printf(vendor_str);
 }
